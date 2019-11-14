5deb55bc3d04cb3846b0177769fc537285a065c7
Jianfeng Li <jianfeng.li@pwc.com>
2019-11-15 07:23:07 +1030

    Try out long run in handler

--- geolocation/server.go
+++ geolocation/server.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
+
+func longTask() {
+	fmt.Println("The long task starts")
+	time.Sleep(1 * time.Second)
+	fmt.Println("The long task finished")
+}
 
 func main() {
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		longTask()
 		w.Header().Set("Content-Type", "application/json")
 		io.WriteString(w, `{"key":"value"}`)
 	}
