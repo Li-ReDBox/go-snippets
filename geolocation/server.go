18f44fd42c5eccd5b554751e1fb24571172dcc3b
Jianfeng Li <jianfeng.li@pwc.com>
2019-11-15 09:35:54 +1030

    Set up a basic background processor

--- geolocation/server.go
+++ geolocation/server.go
@@ -8,19 +8,30 @@ import (
 	"time"
 )
 
-func longTask() {
-	fmt.Println("The long task starts")
-	time.Sleep(1 * time.Second)
-	fmt.Println("The long task finished")
+func longRunTask(c chan bool) {
+	// Demo a background processor
+	for {
+		select {
+		case <-c:
+			fmt.Println("The long task starts")
+			time.Sleep(1 * time.Second)
+			fmt.Println("The long task finished")
+		case <-time.After(5 * time.Second):
+			fmt.Println(time.Now(), "pong")
+		}
+	}
 }
 
 func main() {
+	c := make(chan bool)
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		longTask()
+		fmt.Println("Set c to true to trigger background task runner.")
+		c <- true
 		w.Header().Set("Content-Type", "application/json")
 		io.WriteString(w, `{"key":"value"}`)
 	}
 
+	go longRunTask(c)
 	http.HandleFunc("/hello", helloHandler)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
