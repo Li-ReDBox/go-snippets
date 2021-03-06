cee11b051a8c7ccca1ba2784ee8c5f8f96df6d7f
Jianfeng Li <jianfeng.li@pwc.com>
2021-03-07 08:54:01 +1030

    Make more sense in single_bg.go

--- goroutines/single_bg.go
+++ goroutines/single_bg.go
@@ -1,67 +1,96 @@
+// Package main contains code of an example with one routine
+// runs in background. We use un-buffer channels, so they work
+// in sequences. But whether all asked jobs can be finished is not certain.
 package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
+// count is a counter of tasks
 var count int
+
+// msg is a channel for sending and receiving messages
 var msg = make(chan string)
 
+// When package starts, run independent in background. It communicates
+// through channel msg
 func init() {
 	go independent(msg)
 }
 
-// Two forms of normal go routines, no difference in functionality
-func background(c chan bool) {
+// run reads from a channel forever
+// Here we show two forms of reading a channel with or without select.
+// There is no difference in functionality.
+func run(c chan bool) {
 	for _ = range c {
 		count += 1
-		fmt.Println(count, ": We were asked to run")
+		log.Println(count, ": job is running ...")
 	}
 }
 
-// there is no difference between background and bkSelect
-func bkSelect(c chan bool) {
+// runSelect checks a channel using select. Here has only one channel, so
+// it is not a correct use of it.
+// there is no difference between run and runSelect
+func runSelect(c chan bool) {
 	for {
 		select {
 		case <-c:
 			count += 1
-			fmt.Println(count, ": We were asked to run")
+			log.Println(count, ": job is running ...")
 		}
 	}
 }
 
+// independent runs in background when package is loaded.
+// it has default case, so it is non-block.
 // package's go routine, started in init()
 func independent(c chan string) {
 	for {
 		select {
 		case w := <-c:
-			fmt.Println("I was asked to spread:", w)
+			log.Println("I was asked to spread message:", w)
 			// pretent this is a heavy func
 			time.Sleep(500 * time.Nanosecond)
-			fmt.Printf("\t\tJob is done: content %s has been spread.\n", w)
+			log.Printf("\t\tJob is done: content `%s` has been spread.\n", w)
 		default:
-			fmt.Println("I am awake to do my stuff... Done... See you in 500ns.")
-			// keep screen clean for a while
-			time.Sleep(500 * time.Nanosecond)
+			fmt.Print(". ")
+			// log.Println("This background stuff will take 500ns to do something.")
+			// // keep screen clean for a while
+			// time.Sleep(500 * time.Nanosecond)
 		}
 	}
 }
 
 func main() {
+	msg <- "Hello, background guard is here to serve the world."
+
+	max := 10
+	fmt.Println("Please set the max times you want to sending message. Default is 10.")
+	fmt.Scanf("%d", &max)
+	fmt.Println("You have set to the max number of run to ", max)
+
+	// set counter to start from 0
 	count = 0
+	// single un-buffered channel
 	con := make(chan bool)
-	go background(con)
+	go run(con)
 	// go bkSelect(con)
-	max := 9
+	// check channel con max times
 	for i := 0; i < max; i++ {
 		con <- true
+		if i%4 == 0 {
+			// tell the background where we are
+			msg <- fmt.Sprintf("Current counter is %d", i)
+		}
 	}
-	msg <- "Fist, world is busy"
 
 	// By comment out sleeping for 100ms, the chance of finishing independent goroutine is reduced.
 	// time.Sleep(100 * time.Microsecond)
-	fmt.Println("After given some spare time to finish, the difference between asked and run =", max-count)
+	log.Println("After given some spare time to finish, the difference between asked and run =", max-count)
 
-	msg <- "Last, world is still busy"
+	// Send message to the independent running in the background
+	msg <- "Last message from background."
 }
